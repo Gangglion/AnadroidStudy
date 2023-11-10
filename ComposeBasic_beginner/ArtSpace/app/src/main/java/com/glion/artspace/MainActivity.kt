780bc110380216168a549f6fe9bab57c07de4c97
shhan <shhan@ubi-plus.com>
2023-11-10 17:01:44 +0900

    Artspace 작업중

--- ComposeBasic_beginner/ArtSpace/app/src/main/java/com/glion/artspace/MainActivity.kt
+++ ComposeBasic_beginner/ArtSpace/app/src/main/java/com/glion/artspace/MainActivity.kt
@@ -8,8 +8,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -20,7 +25,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.glion.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,8 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // TODO : ArtSpaceLayout
-                    ArtSpaceLayout(modifier = Modifier)
+                    ArtSpaceLayout()
                 }
             }
         }
@@ -45,11 +51,15 @@ class MainActivity : ComponentActivity() {
 fun ArtSpaceLayout(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .fillMaxSize()
     ){
         ImageArea()
+        TextArea(title = R.string.title_temp, artist = R.string.artist_temp)
+        Spacer(modifier.height(20.dp))
+        ButtonArea(
+            modifier = modifier
+                .wrapContentHeight(Alignment.Bottom)
+        )
     }
 }
 
@@ -57,12 +67,36 @@ fun ArtSpaceLayout(modifier: Modifier = Modifier) {
 fun ImageArea() {
 
 }
+@Composable
+fun TextArea(
+    modifier: Modifier = Modifier,
+    title: Int,
+    artist: Int
+)
+{
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    ) {
+        Text(
+            text = "ArtWork Title",
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            text = "Artwork Artist (Year)",
+            textAlign = TextAlign.Center
+        )
+    }
+}
 
 @Composable
 fun ButtonArea(modifier: Modifier = Modifier){
     Row(
         modifier = modifier
-            .fillMaxWidth()
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.Bottom
     ){
         Button(
             onClick = {},
@@ -70,14 +104,22 @@ fun ButtonArea(modifier: Modifier = Modifier){
                 .wrapContentWidth(Alignment.Start)
         )
         {
-            Text(text = stringResource(id = R.string.previous))
+            Text(
+                text = stringResource(id = R.string.previous),
+                modifier = Modifier
+                    .padding(start = 20.dp, end = 20.dp)
+            )
         }
         Button(
             onClick = {},
             modifier = modifier
                 .wrapContentWidth(Alignment.End)
         ){
-            Text(text = stringResource(id = R.string.next))
+            Text(
+                text = stringResource(id = R.string.next),
+                modifier = Modifier
+                    .padding(start = 30.dp, end = 30.dp)
+            )
         }
     }
 }
@@ -96,6 +138,5 @@ fun GreetingPreview() {
     ArtSpaceTheme {
         // TODO : ArtSpaceLayout
         ArtSpaceLayout(modifier = Modifier)
-        ButtonArea()
     }
 }
