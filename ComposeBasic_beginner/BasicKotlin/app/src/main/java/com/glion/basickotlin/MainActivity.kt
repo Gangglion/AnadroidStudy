95dc4525a12964fa846f9931fd8a44bca855e5f1
shhan <shhan@ubi-plus.com>
2023-11-15 11:07:13 +0900

    단원 2 코틀린 기본 학습 완료.

--- ComposeBasic_beginner/BasicKotlin/app/src/main/java/com/glion/basickotlin/MainActivity.kt
+++ ComposeBasic_beginner/BasicKotlin/app/src/main/java/com/glion/basickotlin/MainActivity.kt
@@ -26,129 +26,36 @@ class MainActivity : ComponentActivity() {
                 }
             }
 
-            // fun main() 코드를 여기 넣으면됨
-            var smartDevice: SmartDevice = SmartTvDevice("Android TV", "Entertainment")
-            smartDevice.turnOn()
+            // TODO : fun main() 코드를 여기 넣으면됨
+            // MEMO : Smart 코드
+            val smartTvDevice = Smart.SmartTvDevice("Android TV", "Entertainment")
+            smartTvDevice.turnOn()
 
-            smartDevice = SmartLightDevice("Google Light", "Utility")
-            smartDevice.turnOn()
-        }
-    }
-}
-open class SmartDevice(val name: String, val category: String){
-    var deviceStatus = "online"
-    constructor(name: String, category: String, statusCode: Int): this(name, category){
-        deviceStatus = when(statusCode){
-            0 -> "offline"
-            1-> "online"
-            else -> "unknown"
-        }
-    }
-    open fun turnOn(){
-        println("Smart device is turned on.")
-    }
+            val smartLightDevice = Smart.SmartLightDevice("Google Light", "Utility")
+            smartLightDevice.turnOn()
 
-    open fun turnOff(){
-        println("Smart device is turned off.")
-    }
-}
+            val smartHome = Smart.SmartHome(smartTvDevice, smartLightDevice)
+            smartHome.printSmartTvInfo()
+            smartHome.printSmartLightInfo()
 
-class SmartTvDevice(deviceName: String, deviceCategory: String) : SmartDevice(name = deviceName, category = deviceCategory){
-    var speakerVolume = 2
-        set(value){
-            if(value in 0..100){
-                field = value
+            // LaMDA 코드
+            val coins: (Int) -> String = { quantity ->
+                "$quantity quarters" // MEMO : 반환되는 String 값. 따로 return이 존재하진 않는다.
             }
-        }
-    var channelNumber = 1
-        set(value){
-            if(value in 0..200){
-                field = value
+            val cupcake: (Int) -> String = {
+                "Have a cupcake!"
             }
-        }
-
-    override fun turnOn() {
-        deviceStatus = "on"
-        println(
-            "$name is turned on. Speaker volume is set to $speakerVolume and channel number is " +
-                    "set to $channelNumber."
-        )
-    }
-
-    override fun turnOff() {
-        deviceStatus = "off"
-        println("$name turned off")
-    }
-
-    fun increaseSpeakerVolume(){
-        speakerVolume++
-        println("Speaker Volume increased to $speakerVolume.")
-    }
-
-    fun nextChannel(){
-        channelNumber++
-        println("Channel number increased to $channelNumber.")
-    }
-}
-
-class SmartLightDevice(deviceName: String, deviceCategory: String) : SmartDevice(name = deviceName, category = deviceCategory){
-    var brightnessLevel = 0
-        set(value) {
-            if (value in 0..100) {
-                field = value
+            val treatFunction = Lamda.trickOrTreat(false, coins) // MEMO : 함수를 실행한 것이 아니라 참조한 것이므로 결과값이 나오지 않음
+            // MEMO : coins 자리에 결과인 { $it quarters } 를 사용할 수도 있다.
+            val treatFunction2 = Lamda.trickOrTreat(false, { "$it quearters" })
+            // MEMO : 함수의 매개변수 마지막이 람다표현식일 경우 밖으로 뺄 수 있다. 의미는 동일하다.
+            val treatFunction3 = Lamda.trickOrTreat(false) { "$it quearters" }
+            val trickFunction = Lamda.trickOrTreat(true, null)
+            repeat(4){ // MEMO : for문을 간결하게 표현할 수 있음.
+                trickFunction()
             }
+            treatFunction()
         }
-
-    override fun turnOn() {
-        deviceStatus = "on"
-        brightnessLevel = 2
-        println("$name turned on. The brightness level is $brightnessLevel.")
-    }
-
-    override fun turnOff() {
-        deviceStatus = "off"
-        brightnessLevel = 0
-        println("Smart Light turned off")
-    }
-
-    fun increaseBrightness() {
-        brightnessLevel++
-        println("Brightness increased to $brightnessLevel.")
-    }
-}
-
-class SmartHome(val smartTvDevice: SmartTvDevice, val smartLightDevice: SmartLightDevice){
-    fun turnOnTv(){
-        smartTvDevice.turnOn()
-    }
-
-    fun turnOffTv(){
-        smartTvDevice.turnOff()
-    }
-
-    fun increaseTvVolume() {
-        smartTvDevice.increaseSpeakerVolume()
-    }
-
-    fun changeTvChannelToNext() {
-        smartTvDevice.nextChannel()
-    }
-
-    fun turnOnLight() {
-        smartLightDevice.turnOn()
-    }
-
-    fun turnOffLight() {
-        smartLightDevice.turnOff()
-    }
-
-    fun increaseLightBrightness() {
-        smartLightDevice.increaseBrightness()
-    }
-
-    fun turnOffAllDevices(){
-        turnOffTv()
-        turnOffLight()
     }
 }
 
