950e3fdaa63de7a2f0cb44aa3dc12993a95aa76e
shhan <shhan@ubi-plus.com>
2023-11-16 16:42:02 +0900

    GridBuild 정답 코드 복붙 / 주석 추가. 해상도별 dimens 추가

--- ComposeBasic_beginner/Gridbuild/app/src/main/java/com/glion/gridbuild/MainActivity.kt
+++ ComposeBasic_beginner/Gridbuild/app/src/main/java/com/glion/gridbuild/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,13 +26,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.glion.gridbuild.data.GridDataSource
-import com.glion.gridbuild.model.CardData
-import com.glion.gridbuild.model.GridRowData
+import com.glion.gridbuild.data.DataSource
+import com.glion.gridbuild.model.Topic
 import com.glion.gridbuild.ui.theme.GridbuildTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,12 +40,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GridbuildTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GridCardList(GridDataSource().getData())
+                    TopicGrid(
+                        modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small)) // MEMO : dimens.xml 에 padding 값 넣어줌.
+                    )
                 }
             }
         }
@@ -45,66 +54,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GridCardList(
-    gridRowData: List<GridRowData>,
-    modifier: Modifier = Modifier
-){
-    LazyColumn(
+fun TopicGrid(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2), // MEMO : 한 행당 열수 제한. 원하는 인자값으로 넣어 한 행당 지정한 개수의 항목 들어오게됨
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
         modifier = modifier
-    ){
-        items(gridRowData){gridData ->
-            GridCardRow(gridData = gridData)
+    ) {
+        items(DataSource.topics) { topic ->
+            TopicCard(topic)
         }
     }
 }
 
+// MEMO : 카드 한개의 UI 를 그리는 컴포저블
 @Composable
-fun GridCardRow(
-    gridData: GridRowData,
-    modifier: Modifier = Modifier
-){
-    Row(
-        modifier = modifier
-            .padding(start = 8.dp, top = 8.dp, end = 8.dp)
-    ) {
-        CardUI(gridData.cardData1, modifier.weight(1f))
-        CardUI(gridData.cardData2,
-            modifier
-                .weight(1f)
-                .padding(start = 8.dp))
-    }
-}
-
-@Composable
-fun CardUI(data: CardData, modifier: Modifier = Modifier){
-    Card(
-        modifier = modifier
-    ) {
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card {
         Row {
+            // MEMO : 원래는 하단의 Image 컴포저블이 Box 로 쌓여있었는데, 쌓여있지 않아도 상관없는것처럼 보인다.
             Image(
-                painter = painterResource(id = data.drawableRes),
-                contentScale = ContentScale.Crop,
-                contentDescription = null
+                painter = painterResource(id = topic.imageRes),
+                contentDescription = null,
+                modifier = modifier
+                    .size(width = 68.dp, height = 68.dp) // MEMO : 이미지 컴포저블의 사이즈
+                    .aspectRatio(1f), // MEMO : 위에서 지정한 이미지 사이즈에 꽉 차도록 1대1 비율로 이미지 자체의 크기를 조정하기 위해 사용됨
+                contentScale = ContentScale.Crop // MEMO : 이미지의 너비와 높이가 대상(Image Composable 의 사이즈)의 치수와 같거나 더 크도록 소스를 균일하게 조정함(종횡비 유지)
             )
-            Column(
-                modifier = Modifier
-                    .padding(8.dp)
-            ){
+
+            Column {
                 Text(
-                    text = stringResource(id = data.stringRes),
-                    style = MaterialTheme.typography.bodySmall
+                    text = stringResource(id = topic.name),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(R.dimen.padding_medium),
+                        top = dimensionResource(R.dimen.padding_medium),
+                        end = dimensionResource(R.dimen.padding_medium),
+                        bottom = dimensionResource(R.dimen.padding_small)
+                    )
                 )
-                Row(
-                    modifier = Modifier
-                        .padding(top = 4.dp)
-                ){
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_grain),
-                        contentDescription = null
+                Row(verticalAlignment = Alignment.CenterVertically) { // MEMO : 하위요소를 정렬한다. 수직기준 중앙으로(Text나 Icon 이 영향을 받는다)
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = dimensionResource(R.dimen.padding_medium))
                     )
                     Text(
-                        text = data.count.toString(),
-                        style = MaterialTheme.typography.labelSmall
+                        text = topic.availableCourses.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_small))
                     )
                 }
             }
@@ -116,12 +115,13 @@ fun CardUI(data: CardData, modifier: Modifier = Modifier){
 @Composable
 fun GridCardRowPreview(){
     GridbuildTheme {
-        // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            GridCardList(GridDataSource().getData())
+            TopicGrid(
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small))
+            )
         }
     }
 }
