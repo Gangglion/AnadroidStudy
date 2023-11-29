585748afe59743c0c3e23d4bb63065dcacc237f2
SeungHyeob Han <o0tmdguq0o@gmail.com>
2023-11-29 16:40:23 +0900

    [단원 4-3] Sports widthDp 따라 화면 구성 변경되게 수정. "연습 : Sports 앱 빌드" 과정 완료

--- ComposeBasic_beginner/Sports/app/src/main/java/com/glion/sports/MainActivity.kt
+++ ComposeBasic_beginner/Sports/app/src/main/java/com/glion/sports/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,15 +17,45 @@ import com.glion.sports.ui.SportsApp
 import com.glion.sports.ui.theme.SportsTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             SportsTheme {
+                val windowSize = calculateWindowSizeClass(activity = this).widthSizeClass
                 Surface {
-                    SportsApp()
+                    SportsApp(windowSize)
                 }
             }
+        }
+    }
+}
+
+@Preview(widthDp = 400)
+@Composable
+fun CompactSportsAppPreview(){
+    SportsTheme {
+        Surface {
+            SportsApp(WindowWidthSizeClass.Compact)
+        }
+    }
+}
+@Preview(widthDp = 700)
+@Composable
+fun MediumSportsAppPreview(){
+    SportsTheme {
+        Surface {
+            SportsApp(WindowWidthSizeClass.Medium)
+        }
+    }
+}
+@Preview(widthDp = 1000)
+@Composable
+fun ExpandedSportsAppPreview(){
+    SportsTheme {
+        Surface {
+            SportsApp(WindowWidthSizeClass.Expanded)
         }
     }
 }
