0d6ece04993eb05472079fad95391c45b0c1b213
SeungHyeob Han <o0tmdguq0o@gmail.com>
2023-12-19 16:07:21 +0900

    Fix : RealmRepository coroutine 사용 변경
    코루틴 사용 방법에 대해 변경하지 않아도 되므로 원래대로 복원

--- MVVMActivity/app/src/main/java/com/example/mvvmactivity/data/local/repository/RealmRepository.kt
+++ MVVMActivity/app/src/main/java/com/example/mvvmactivity/data/local/repository/RealmRepository.kt
@@ -10,38 +10,34 @@ class RealmRepository {
     /**
      * Realm Data Read
      */
-    suspend fun getAllData(): List<RealmData>{
-        return withContext(Dispatchers.IO){
-            val realm = Realm.getDefaultInstance()
-            val data = realm.where(RealmData::class.java).findAll()
-            val result = realm.copyFromRealm(data)
-            realm.close()
-            result
-        }
+    suspend fun getAllData(): List<RealmData> = withContext(Dispatchers.IO) {
+        val realm = Realm.getDefaultInstance()
+        val data = realm.where(RealmData::class.java).findAll()
+        val result = realm.copyFromRealm(data)
+        realm.close()
+        result
     }
+
     /**
      * Realm Data Write
      */
-    suspend fun insertOrUpdateData(data: RealmData){
-        return withContext(Dispatchers.IO) {
-            val realm = Realm.getDefaultInstance()
-            realm.executeTransaction{
-                it.copyToRealmOrUpdate(data) // Realm 데이터 객체 RealmData에 적용시킴.
-            }
-            realm.close()
+    suspend fun insertOrUpdateData(data: RealmData) = withContext(Dispatchers.IO) {
+        val realm = Realm.getDefaultInstance()
+        realm.executeTransaction {
+            it.copyToRealmOrUpdate(data) // Realm 데이터 객체 RealmData에 적용시킴.
         }
+        realm.close()
     }
+
     /**
      * Realm Data Delete
      */
-    suspend fun deleteAllData(){
-        return withContext(Dispatchers.IO){
-            val realm = Realm.getDefaultInstance()
-            realm.executeTransaction {
-                val willDeleteData = realm.where(RealmData::class.java).findAll()
-                willDeleteData.deleteAllFromRealm()
-            }
-            realm.close()
+    suspend fun deleteAllData() = withContext(Dispatchers.IO) {
+        val realm = Realm.getDefaultInstance()
+        realm.executeTransaction {
+            val willDeleteData = realm.where(RealmData::class.java).findAll()
+            willDeleteData.deleteAllFromRealm()
         }
+        realm.close()
     }
 }
