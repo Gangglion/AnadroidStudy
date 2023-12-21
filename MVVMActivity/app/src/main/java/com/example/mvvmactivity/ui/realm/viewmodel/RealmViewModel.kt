7038331b4b056f3e79c8744129ecd5ee9d5cfc5c
SeungHyeob Han <o0tmdguq0o@gmail.com>
2023-12-21 11:03:49 +0900

    Feat : TextInputLayout, TextInputEditText 추가. 양방향 데이터바인딩 추가
    사용해보지 못했던 TextView를 사용, EditText에 입력한 값을 DB에 저장할 수 있도록 변경. 양방향 데이터바인딩 적용

--- MVVMActivity/app/src/main/java/com/example/mvvmactivity/ui/realm/viewmodel/RealmViewModel.kt
+++ MVVMActivity/app/src/main/java/com/example/mvvmactivity/ui/realm/viewmodel/RealmViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.mvvmactivity.R
+import com.example.mvvmactivity.data.local.model.RealmData
 import com.example.mvvmactivity.data.local.repository.RealmRepository
 import com.example.mvvmactivity.ui.base.BaseViewModel
 import com.example.mvvmactivity.ui.realm.model.TextViewData
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class RealmViewModel(
@@ -14,6 +16,11 @@ class RealmViewModel(
 ) : BaseViewModel() {
     private val _dataList: MutableLiveData<String> = MutableLiveData()
     val dataList: LiveData<String> = _dataList
+    // 양방향 데이터 바인딩 - EditText
+    // MEMO : LiveData 형식은 양방향 데이터 바인딩이 가능하지 않음. 왜? 변경이 되지 않기 때문.
+    //  기존에 _value, value 형식은 viewModel 내부에서 _value 를 변경하고, viewModel 외부에서 관측하기 위함이였지만,
+    //  양방향 데이터 바인딩은 layout과 viewModel 간 데이터가 양방향으로 변경되기 때문에 변경이 불가능한 LiveData 형식으로는 불가능하다.
+    val inputData: MutableLiveData<String> = MutableLiveData("")
 
     fun getAllData(){
         viewModelScope.launch{
@@ -36,6 +43,20 @@ class RealmViewModel(
             repository.deleteAllData()
             val list = repository.getAllData()
             _dataList.value = list.toString()
+        }
+    }
+
+    fun inputData(){
+        if(inputData.value!!.isNotEmpty()){
+            val insertData = RealmData(index = 28, title = inputData.value)
+            viewModelScope.launch(exceptionHandler) {
+                val deferred = async{
+                    repository.insertOrUpdateData(insertData)
+                }
+                deferred.await()
+                getAllData()
+                inputData.value = ""
+            }
         }
     }
 }
