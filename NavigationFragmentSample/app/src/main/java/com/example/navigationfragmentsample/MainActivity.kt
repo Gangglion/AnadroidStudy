e334238ebf880c6c8a0b5b52d86568b28f908ce7
shhan <shhan@ubi-plus.com>
2024-12-27 16:11:07 +0900

    update : [NavigationFragment] 딥링크 생성 연습

--- NavigationFragmentSample/app/src/main/java/com/example/navigationfragmentsample/MainActivity.kt
+++ NavigationFragmentSample/app/src/main/java/com/example/navigationfragmentsample/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.navigationfragmentsample
 
+import android.app.ComponentCaller
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,27 +16,7 @@ import com.example.navigationfragmentsample.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-    private var lastBackPressed: Long? = null
 
-    private var backPressedCallback = object : OnBackPressedCallback(true) {
-        override fun handleOnBackPressed() {
-            if(navController.currentDestination!!.id != R.id.fragmentGraphMain) {
-                navController.navigateUp()
-            } else {
-                if(lastBackPressed == null) {
-                    lastBackPressed = System.currentTimeMillis()
-                    Toast.makeText(this@MainActivity, "뒤로 버튼을 한번 더 누르시면 앱을 종료합니다", Toast.LENGTH_SHORT).show()
-                } else {
-                    if(System.currentTimeMillis() - lastBackPressed!! < 2000) {
-                        finish()
-                    } else {
-                        Toast.makeText(this@MainActivity, "뒤로 버튼을 한번 더 누르시면 앱을 종료합니다", Toast.LENGTH_SHORT).show()
-                        lastBackPressed = System.currentTimeMillis()
-                    }
-                }
-            }
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -46,9 +26,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0) // note : BottomNavigation 에 패딩이 적용되는 것을 막기 위해 systemBars.bottom 대신 0dp 로 지정
             insets
         }
-        onBackPressedDispatcher.addCallback(backPressedCallback)
         setMainNav()
         setBottomNavigation()
+
+        intent?.let {
+            navController.handleDeepLink(it)
+        }
     }
 
     private fun setMainNav() {
--- NavigationFragmentSample/app/src/main/java/com/example/navigationfragmentsample/deeplink/DeeplinkFragment.kt
+++ NavigationFragmentSample/app/src/main/java/com/example/navigationfragmentsample/deeplink/DeeplinkFragment.kt
@@ -1,23 +1,37 @@
 package com.example.navigationfragmentsample.deeplink
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.navigationfragmentsample.R
+import com.example.navigationfragmentsample.databinding.FragmentDeeplinkBinding
 
 class DeeplinkFragment : Fragment() {
+    private lateinit var binding: FragmentDeeplinkBinding
+    private var mTitle: String = ""
+    private var mContents: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mTitle = arguments?.getString("title") ?: "null"
+        mContents = arguments?.getString("contents") ?: "null"
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_deeplink, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_deeplink, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvTitle.text = mTitle
+        binding.tvContents.text = mContents
+    }
 }
